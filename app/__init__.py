6a9262bd954b60d016cf8e35cec558ff1bda1b62
sidsun1 <siddharthsundar2@gmail.com>
2025-06-20 18:31:14 -0700

    Modularized loading data from JSON files

--- app/__init__.py
+++ app/__init__.py
@@ -12,47 +12,37 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
 
+def load_json_data(path: Path, category: str) -> list:
+    try:
+        with open(path, 'r', encoding = 'utf-8') as f:
+            contents = json.load(f)
+    except Exception as e:
+        contents = []
+        print(f'Error loading {category}: {e}')
+    return contents
+
 @app.route('/work')
 def work():
     path = Path('app/static/json-data/experiences.json')
-
-    try:
-        with open(path, 'r', encoding = 'utf-8') as f:
-            experiences = json.load(f)
-    except Exception as e:
-        experiences = []
-        print(f'Error loading experiences: {e}')
-
-    # print(experiences)
-    
+    experiences = load_json_data(path, 'experiences')
     return render_template('work.html', title="Experience", experiences = experiences, url=os.getenv("URL"))
 
 @app.route('/hobbies')
 def hobbies():
     path = Path('app/static/json-data/hobbies.json')
-
-    try:
-        with open(path, 'r', encoding='utf-8') as f:
-            hobbies = json.load(f)
-    except Exception as e:
-        hobbies = []
-        print(f'Error loading hobbies: {e}')
-
+    hobbies = load_json_data(path, 'hobbies')
     return render_template('hobbies.html', title="My Hobbies", description="These are the activities that keep me inspired and energized outside of my professional work.", hobbies=hobbies, url=os.getenv("URL"))
 
 @app.route('/education')
 def education():
+    path = Path('app/static/json-data/education.json')
+
     return render_template('education.html', title="Education", url=os.getenv("URL"))
 
 @app.route('/map')
 def map():
     path = Path('app/static/json-data/places.json')
-    try:
-        with open(path, 'r', encoding='utf-8') as f:
-            places = json.load(f)
-    except Exception as e:
-        places = []
-        print(f'Error loading places: {e}')
+    places = load_json_data(path, 'places')
 
     folium_map = folium.Map(location=[40.7128, -74.0060], zoom_start=3, min_zoom=3)
     add_markers(folium_map, places)
