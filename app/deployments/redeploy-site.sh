89a11632d0e4d8e41592392afb55e38d0b59bfae
sidsun1 <siddharthsundar2@gmail.com>
2025-07-05 05:05:01 +0000

    Added basic bash deployment script

---
+++ app/deployments/redeploy-site.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+tmux kill-server
+
+cd ..
+git fetch && git reset origin/main --hard
+
+cd ..
+source python3-virtualenv/bin/activate
+pip install -r requirements.txt
+
+tmux new-session -d -s flask_server
+export FLASK_APP=app
+flask run --host=0.0.0.0
