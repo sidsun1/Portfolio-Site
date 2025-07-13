f318c4ceafe386bd385d427a45656d19aea42683
sidsun1 <siddharthsundar2@gmail.com>
2025-07-13 21:18:07 +0000

    Changed redeploy-site script to use a service daemon

--- app/deployments/redeploy-site.sh
+++ app/deployments/redeploy-site.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-tmux kill-server
+cd /root/Portfolio-Site
 
-cd ..
 git fetch && git reset origin/main --hard
 
-cd ..
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 
-tmux new-session -d -s flask_server 'export FLASK_APP=app && flask run --host=0.0.0.0'
+sudo systemctl restart myportfolio
