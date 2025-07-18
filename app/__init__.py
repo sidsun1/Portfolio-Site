3200afd2d466df8def214ab6476a8368b7555e89
MiguelR222 <miguel.ruiz0652@gmail.com>
2025-07-18 03:12:34 -0600

    Feat: Add home, timeline and endpoint tests

--- app/__init__.py
+++ app/__init__.py
@@ -13,13 +13,16 @@ app = Flask(__name__)
 
 ### Databases ###
 
-mydb = \
-MySQLDatabase(os.getenv('MYSQL_DATABASE'),                                        
-            user = os.getenv('MYSQL_USER'),
-            password = os.getenv('MYSQL_PASSWORD'),
-            host = os.getenv('MYSQL_HOST'),
-            port = 3306
-)
+if os.getenv("TESTING") == "true":
+    print("Running in test mode")
+    mydb = SqliteDatabase('file:memory?mode=memory&cache=shared', uri=True)
+else:
+    mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD"),
+        host=os.getenv("MYSQL_HOST"),
+        port=3306
+    )
 
 # print(mydb)
 
